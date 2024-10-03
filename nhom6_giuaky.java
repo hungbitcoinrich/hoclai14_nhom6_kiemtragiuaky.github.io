13eb1bc801d4032612542ad7b2d2153b7b56b119
7270-NguyenVietAnh <nguyenvietanh08022003@gmail.com>
2024-10-03 10:51:25 +0700

    them chuc nang chia

--- nhom6_giuaky.java
+++ nhom6_giuaky.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 public class nhom6_giuaky {
 
+    public static double divide(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            System.out.println("Lỗi: Không thể chia cho 0!");
+            return 0; // Trả về 0 khi có lỗi chia cho 0
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -17,13 +25,15 @@ public class nhom6_giuaky {
         System.out.println("3. Nhan (*)");
         System.out.println("4. Chia (/)");
 
+
         int choice = scanner.nextInt();
         double result = 0;
 
         switch (choice) {
-            // case 1:
-            // result = add(num1, num2);
-            // break;
+            case 4:
+                result = divide(num1, num2);
+                System.out.println("Kết quả: " + result);
+            break;
             default:
                 System.out.println("Lựa chọn không hợp lệ!");
                 return;
